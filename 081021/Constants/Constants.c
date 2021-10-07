9fc411d61a6873bdf5109ed56f9b12d20d43fd79
MariaMGV <91821573+MariaMGV@users.noreply.github.com>
2021-10-08 00:28:16 +0300

    Update Constants.c

--- 081021/Constants/Constants.c
+++ 081021/Constants/Constants.c
@@ -8,34 +8,34 @@
 #include <math.h>
 //define PI 3.14159; => PI will be used instead const double g_Pi
 
-double sCircle(float fRadius);
-double sEllips(float A, float B);
-const double g_Pi = 3.14159;
+float sCircle(float fRadius);
+float sEllips(float A, float B);
+const float g_Pi = 3.14159;
 
 int main()
 {
 float fRadius, A, B;
 
 //CIRCLE
-printf("The face of a circle: %.2f\n",sCircle(1));
-printf("The face of a circle: %lf\n",sCircle(1.5));
-printf("The face of a circle: %lf\n\n",sCircle(13));
+printf("The face of a circle: %f\n",sCircle(1));
+printf("The face of a circle: %f\n",sCircle(1.5));
+printf("The face of a circle: %f\n\n",sCircle(13));
 
 // ELLIPSE
-printf("The face of an ellipse: %lf\n",sEllips(5,3));
-printf("The face of an ellipse: %lf\n",sEllips(1.5,1));
-printf("The face of an ellipse: %lf\n",sEllips(13,18.5));
+printf("The face of an ellipse: %f\n",sEllips(5,3));
+printf("The face of an ellipse: %f\n",sEllips(1.5,1));
+printf("The face of an ellipse: %f\n",sEllips(13,18.5));
 
 return 0;
 
 }
 
- double sCircle(float fRadius)
+ float sCircle(float fRadius)
  {
       return ( g_Pi * fRadius * fRadius);
  }
 
- double sEllips(float A, float B)
+ float sEllips(float A, float B)
  {
       return (g_Pi * A * B);
  }
